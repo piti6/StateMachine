dd8a00f4ed21d742a955c2f5e1d1e7844a366ce1
Minhyuk Kim <minhyuk.kim@gree.net>
2020-07-28 21:40:35 +0900

    Fix

---
+++ Extensions/DIContainerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Zenject;
+
+public static class DIContainerExtensions
+{
+    public static NonLazyBinder
+            BindSubContainerFromNewGameObjectInstaller<TController, TInstaller>(this DiContainer container)
+            where TController : ControllerBase
+            where TInstaller : InstallerBase
+    {
+        return container.BindFactory<TController, PlaceholderFactory<TController>>()
+            .FromSubContainerResolve()
+            .ByNewGameObjectInstaller<TInstaller>()
+            .WithGameObjectName(typeof(TController).Name);
+    }
+}
