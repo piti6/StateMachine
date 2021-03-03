cbb15d63cc010d446f7bd999266bb1c95835b2a7
piti6 <piti6@users.noreply.github.com>
2021-03-03 22:49:51 +0900

    Adjust Dynamic prefab async resource factory

--- VContainer/DynamicPrefabAsyncResourceFactory.cs
+++ VContainer/DynamicPrefabAsyncResourceFactory.cs
@@ -1,22 +1,31 @@
-using ModestTree;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
-using Zenject;
+using VContainer;
 
 public class DynamicPrefabAsyncResourceFactory
 {
     [Inject]
-    private readonly DiContainer _container = default;
+    private readonly Container _container = default;
 
     public async UniTask<T> CreateAsync<T>(string prefabPath) where T : Component
     {
-        Assert.That(!string.IsNullOrEmpty(prefabPath),
-          "Null or empty prefab resource name given to factory create method when instantiating object with type '{0}'.", typeof(T));
+        Debug.Assert(!string.IsNullOrEmpty(prefabPath), $"Null or empty prefab resource name given to factory create method when instantiating object path {prefabPath} with type '{typeof(T)}'.");
+
+        var instance = await CreateAsync(prefabPath);
+        return instance.GetComponent<T>();
+    }
+
+    public async UniTask<GameObject> CreateAsync(string prefabPath)
+    {
+        Debug.Assert(!string.IsNullOrEmpty(prefabPath), $"Null or empty prefab resource name given to factory create method when instantiating object path {prefabPath}.");
 
         var prefab = await Resources.LoadAsync<GameObject>(prefabPath);
 
-        Assert.IsNotNull(prefab, $"Could not find prefab from path: {prefabPath}");
+        Debug.Assert(prefab, $"Could not find prefab from path: {prefabPath}");
 
-        return _container.InstantiatePrefabForComponent<T>(prefab);
+        var instance = Object.Instantiate(prefab) as GameObject;
+        _container.Inject(instance);
+
+        return instance;
     }
 }
